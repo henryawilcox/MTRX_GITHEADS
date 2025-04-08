0fa1d0da5a3e03853d07ffe2e738bfb228008456
“[firstname <[valid-email]henryatticuswilcox@gmail.com>
2025-04-08 14:51:51 +1000

    new files

---
+++ DigitalIO/Debug/Src/main.d
@@ -0,0 +1,15 @@
+Src/main.o: ../Src/main.c \
+ /Users/henrywilcox/MTRX2700-2025/stm32f303-definitions/Core/Inc/stm32f303xc.h \
+ /Users/henrywilcox/MTRX2700-2025/stm32f303-definitions/Core/Inc/core_cm4.h \
+ /Users/henrywilcox/MTRX2700-2025/stm32f303-definitions/Core/Inc/cmsis_version.h \
+ /Users/henrywilcox/MTRX2700-2025/stm32f303-definitions/Core/Inc/cmsis_compiler.h \
+ /Users/henrywilcox/MTRX2700-2025/stm32f303-definitions/Core/Inc/cmsis_gcc.h \
+ /Users/henrywilcox/MTRX2700-2025/stm32f303-definitions/Core/Inc/mpu_armv7.h \
+ /Users/henrywilcox/MTRX2700-2025/stm32f303-definitions/Core/Inc/system_stm32f3xx.h
+/Users/henrywilcox/MTRX2700-2025/stm32f303-definitions/Core/Inc/stm32f303xc.h:
+/Users/henrywilcox/MTRX2700-2025/stm32f303-definitions/Core/Inc/core_cm4.h:
+/Users/henrywilcox/MTRX2700-2025/stm32f303-definitions/Core/Inc/cmsis_version.h:
+/Users/henrywilcox/MTRX2700-2025/stm32f303-definitions/Core/Inc/cmsis_compiler.h:
+/Users/henrywilcox/MTRX2700-2025/stm32f303-definitions/Core/Inc/cmsis_gcc.h:
+/Users/henrywilcox/MTRX2700-2025/stm32f303-definitions/Core/Inc/mpu_armv7.h:
+/Users/henrywilcox/MTRX2700-2025/stm32f303-definitions/Core/Inc/system_stm32f3xx.h:
