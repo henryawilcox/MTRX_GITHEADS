b31e88b45c875b851ac6195a2359caaca98aecbe
“[firstname <[valid-email]henryatticuswilcox@gmail.com>
2025-04-09 12:00:04 +1000

    update

---
+++ DigitalIO/Inc/digital_io.h
@@ -0,0 +1,10 @@
+#ifndef DIGITAL_H_
+#define DIGITAL_H_
+
+#include <stdint.h>
+
+/**
+ * Initialize digital I/O module with a callback for button press
+ * on_button_press Function to call when button is pressed
+ */
+void DigitalInitialise(void (*callback)());
