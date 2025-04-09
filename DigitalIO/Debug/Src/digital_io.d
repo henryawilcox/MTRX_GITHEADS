47ab1a632f411bbc0331864ed715546b5a1fbccf
“[firstname <[valid-email]henryatticuswilcox@gmail.com>
2025-04-09 16:21:23 +1000

    inc files

--- DigitalIO/Debug/Src/digital_io.d
+++ DigitalIO/Debug/Src/digital_io.d
@@ -1,16 +1,12 @@
 Src/digital_io.o: ../Src/digital_io.c ../Inc/digital_io.h \
- /Users/henrywilcox/MTRX2700-2025/stm32f303-definitions/Core/Inc/stm32f303xc.h \
- /Users/henrywilcox/MTRX2700-2025/stm32f303-definitions/Core/Inc/core_cm4.h \
- /Users/henrywilcox/MTRX2700-2025/stm32f303-definitions/Core/Inc/cmsis_version.h \
- /Users/henrywilcox/MTRX2700-2025/stm32f303-definitions/Core/Inc/cmsis_compiler.h \
- /Users/henrywilcox/MTRX2700-2025/stm32f303-definitions/Core/Inc/cmsis_gcc.h \
- /Users/henrywilcox/MTRX2700-2025/stm32f303-definitions/Core/Inc/mpu_armv7.h \
- /Users/henrywilcox/MTRX2700-2025/stm32f303-definitions/Core/Inc/system_stm32f3xx.h
+ ../Inc/stm32f303xc.h ../Inc/core_cm4.h ../Inc/cmsis_version.h \
+ ../Inc/cmsis_compiler.h ../Inc/cmsis_gcc.h ../Inc/mpu_armv7.h \
+ ../Inc/system_stm32f3xx.h
 ../Inc/digital_io.h:
-/Users/henrywilcox/MTRX2700-2025/stm32f303-definitions/Core/Inc/stm32f303xc.h:
-/Users/henrywilcox/MTRX2700-2025/stm32f303-definitions/Core/Inc/core_cm4.h:
-/Users/henrywilcox/MTRX2700-2025/stm32f303-definitions/Core/Inc/cmsis_version.h:
-/Users/henrywilcox/MTRX2700-2025/stm32f303-definitions/Core/Inc/cmsis_compiler.h:
-/Users/henrywilcox/MTRX2700-2025/stm32f303-definitions/Core/Inc/cmsis_gcc.h:
-/Users/henrywilcox/MTRX2700-2025/stm32f303-definitions/Core/Inc/mpu_armv7.h:
-/Users/henrywilcox/MTRX2700-2025/stm32f303-definitions/Core/Inc/system_stm32f3xx.h:
+../Inc/stm32f303xc.h:
+../Inc/core_cm4.h:
+../Inc/cmsis_version.h:
+../Inc/cmsis_compiler.h:
+../Inc/cmsis_gcc.h:
+../Inc/mpu_armv7.h:
+../Inc/system_stm32f3xx.h:
--- DigitalIO/Debug/Src/main.d
+++ DigitalIO/Debug/Src/main.d
@@ -1,16 +1,11 @@
-Src/main.o: ../Src/main.c ../Inc/digital_io.h \
- /Users/henrywilcox/MTRX2700-2025/stm32f303-definitions/Core/Inc/stm32f303xc.h \
- /Users/henrywilcox/MTRX2700-2025/stm32f303-definitions/Core/Inc/core_cm4.h \
- /Users/henrywilcox/MTRX2700-2025/stm32f303-definitions/Core/Inc/cmsis_version.h \
- /Users/henrywilcox/MTRX2700-2025/stm32f303-definitions/Core/Inc/cmsis_compiler.h \
- /Users/henrywilcox/MTRX2700-2025/stm32f303-definitions/Core/Inc/cmsis_gcc.h \
- /Users/henrywilcox/MTRX2700-2025/stm32f303-definitions/Core/Inc/mpu_armv7.h \
- /Users/henrywilcox/MTRX2700-2025/stm32f303-definitions/Core/Inc/system_stm32f3xx.h
+Src/main.o: ../Src/main.c ../Inc/digital_io.h ../Inc/stm32f303xc.h \
+ ../Inc/core_cm4.h ../Inc/cmsis_version.h ../Inc/cmsis_compiler.h \
+ ../Inc/cmsis_gcc.h ../Inc/mpu_armv7.h ../Inc/system_stm32f3xx.h
 ../Inc/digital_io.h:
-/Users/henrywilcox/MTRX2700-2025/stm32f303-definitions/Core/Inc/stm32f303xc.h:
-/Users/henrywilcox/MTRX2700-2025/stm32f303-definitions/Core/Inc/core_cm4.h:
-/Users/henrywilcox/MTRX2700-2025/stm32f303-definitions/Core/Inc/cmsis_version.h:
-/Users/henrywilcox/MTRX2700-2025/stm32f303-definitions/Core/Inc/cmsis_compiler.h:
-/Users/henrywilcox/MTRX2700-2025/stm32f303-definitions/Core/Inc/cmsis_gcc.h:
-/Users/henrywilcox/MTRX2700-2025/stm32f303-definitions/Core/Inc/mpu_armv7.h:
-/Users/henrywilcox/MTRX2700-2025/stm32f303-definitions/Core/Inc/system_stm32f3xx.h:
+../Inc/stm32f303xc.h:
+../Inc/core_cm4.h:
+../Inc/cmsis_version.h:
+../Inc/cmsis_compiler.h:
+../Inc/cmsis_gcc.h:
+../Inc/mpu_armv7.h:
+../Inc/system_stm32f3xx.h:
