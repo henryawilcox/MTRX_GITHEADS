bd7454f0ebc2d7ff690bf65f11ba7078c11e62d9
rroruu <roryleahclements@gmail.com>
2025-04-14 21:42:34 +1000

    entirely complete timer code & integration example

--- ctimertinkerer/Src/timer.c
+++ ctimertinkerer/Src/timer.c
@@ -2,9 +2,9 @@
 #include "stm32f303xc.h"
 
 static TimerCallback periodicCallback = 0;
-static uint32_t timerPeriod_ms = 0;
-
 static TimerCallback oneShotCallback = 0;
+
+static uint32_t timerPeriod_ms = 0;
 static uint8_t isOneShotMode = 0;
 
 void Timer_Init(uint32_t period_ms, TimerCallback cb) {
@@ -12,30 +12,38 @@ void Timer_Init(uint32_t period_ms, TimerCallback cb) {
     periodicCallback = cb;
 
     RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;
-    TIM2->PSC = 8000 - 1;   // 64 MHz / 64000 = 1 kHz (1 tick = 1 ms)
-    TIM2->ARR = timerPeriod_ms;
 
+    TIM2->PSC = 8000 - 1;  // 1 ms tick
+    TIM2->ARR = timerPeriod_ms;
+    TIM2->CNT = 0;
+
+    TIM2->DIER |= TIM_DIER_UIE;  // Enable update interrupt
     TIM2->EGR |= TIM_EGR_UG;
     TIM2->SR &= ~TIM_SR_UIF;
-    TIM2->CR1 |= TIM_CR1_CEN;
+
+    NVIC_EnableIRQ(TIM2_IRQn);
 }
 
 void Timer_Start(void) {
-    while (1) {
-        if (TIM2->SR & TIM_SR_UIF) { //"Has the timer expired?"
-            TIM2->SR &= ~TIM_SR_UIF; //clears the update flag
+    isOneShotMode = 0;
+    TIM2->CR1 |= TIM_CR1_CEN;
+}
 
-            if (isOneShotMode) { //Checks whether we're in one-shot mode (trigger a callback once, then stop).
-                if (oneShotCallback) oneShotCallback(); //If a valid callback function is stored, call it.
-                oneShotCallback = 0; //set callback to 0
-                isOneShotMode = 0; //keep going
-                TIM2->CR1 &= ~TIM_CR1_CEN; // Stop timer
-                return;
-            }
+void Timer_StartOneShot(uint32_t delay_ms, TimerCallback cb) {
+    isOneShotMode = 1;
+    oneShotCallback = cb;
 
-            if (periodicCallback) periodicCallback();
-        }
-    }
+    TIM2->CR1 &= ~TIM_CR1_CEN;      // Stop timer before configuring
+    TIM2->PSC = 8000 - 1;           // 1 ms tick
+    TIM2->ARR = delay_ms;
+    TIM2->CNT = 0;
+
+    TIM2->DIER |= TIM_DIER_UIE;
+    TIM2->EGR |= TIM_EGR_UG;
+    TIM2->SR &= ~TIM_SR_UIF;
+
+    NVIC_EnableIRQ(TIM2_IRQn);
+    TIM2->CR1 |= TIM_CR1_CEN;
 }
 
 void setTimerPeriod(uint32_t new_period_ms) {
@@ -48,57 +56,17 @@ uint32_t getTimerPeriod(void) {
     return timerPeriod_ms;
 }
 
-//void Timer_StartOneShot(uint32_t delay_ms, TimerCallback cb) {
-//    oneShotCallback = cb;
-//    isOneShotMode = 1;
-//
-//    TIM2->PSC = 8000 - 1;    // 1 ms tick
-//    TIM2->ARR = delay_ms;
-//    TIM2->CNT = 0;
-//
-//    TIM2->EGR |= TIM_EGR_UG;
-//    TIM2->SR &= ~TIM_SR_UIF;
-//    TIM2->CR1 |= TIM_CR1_CEN;
-//
-//    // Blocking wait for one-shot to complete
-//    while (1) {
-//        if (TIM2->SR & TIM_SR_UIF) {
-//            TIM2->SR &= ~TIM_SR_UIF;
-//
-//            if (oneShotCallback) oneShotCallback();
-//            oneShotCallback = 0;
-//            isOneShotMode = 0;
-//
-//            TIM2->CR1 &= ~TIM_CR1_CEN; // Stop timer
-//            return;
-//        }
-//    }
-//}
+void TIM2_IRQHandler(void) {
+    if (TIM2->SR & TIM_SR_UIF) {
+        TIM2->SR &= ~TIM_SR_UIF;
 
-void Timer_StartOneShot(uint32_t delay_ms, TimerCallback cb) {
-    oneShotCallback = cb;
-    isOneShotMode = 1;
-
-    TIM2->CR1 &= ~TIM_CR1_CEN;      // Stop timer before config
-    TIM2->PSC = 8000 - 1;           // 1 ms tick
-    TIM2->ARR = delay_ms;
-    TIM2->CNT = 0;
-
-    TIM2->EGR |= TIM_EGR_UG;        // Force update
-    TIM2->SR &= ~TIM_SR_UIF;
-    TIM2->CR1 |= TIM_CR1_CEN;       // Start timer
-
-    // Blocking wait for one-shot to complete
-    while (1) {
-        if (TIM2->SR & TIM_SR_UIF) {
-            TIM2->SR &= ~TIM_SR_UIF;
-
+        if (isOneShotMode) {
             if (oneShotCallback) oneShotCallback();
             oneShotCallback = 0;
             isOneShotMode = 0;
-
             TIM2->CR1 &= ~TIM_CR1_CEN; // Stop timer
-            return;
+        } else {
+            if (periodicCallback) periodicCallback();
         }
     }
 }
