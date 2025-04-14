74871a15d95666b35d0be97a1199a2395a44fbd4
“[firstname <[valid-email]henryatticuswilcox@gmail.com>
2025-04-14 12:13:02 +1000

    upgraded uart module

--- SerialInterface/Debug/Src/serial.d
+++ SerialInterface/Debug/Src/serial.d
@@ -1,7 +1,7 @@
 Src/serial.o: ../Src/serial.c ../Inc/serial.h ../Inc/stm32f303xc.h \
  ../Inc/core_cm4.h ../Inc/cmsis_version.h ../Inc/cmsis_compiler.h \
  ../Inc/cmsis_gcc.h ../Inc/mpu_armv7.h ../Inc/system_stm32f3xx.h \
- ../Inc/stm32f303xc.h ../Inc/core_cm4.h
+ ../Inc/stm32f303xc.h
 ../Inc/serial.h:
 ../Inc/stm32f303xc.h:
 ../Inc/core_cm4.h:
@@ -11,4 +11,3 @@ Src/serial.o: ../Src/serial.c ../Inc/serial.h ../Inc/stm32f303xc.h \
 ../Inc/mpu_armv7.h:
 ../Inc/system_stm32f3xx.h:
 ../Inc/stm32f303xc.h:
-../Inc/core_cm4.h:
