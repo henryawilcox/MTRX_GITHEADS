ca5674bf8711baecac57c382cf3f0cf06d9c3c7a
“[firstname <[valid-email]henryatticuswilcox@gmail.com>
2025-04-15 22:02:30 +1000

    updated comments

--- Integration/Inc/serial_interrupt.h
+++ Integration/Inc/serial_interrupt.h
@@ -8,8 +8,16 @@
 #ifndef SERIAL_INTERRUPT_H_
 #define SERIAL_INTERRUPT_H_
 
+/**
+ * Configures and enables UART interrupts for the specified serial port
+ * serial_port Pointer to the serial port structure to configure
+ */
 void EnableSerialInterrupts(SerialPort *serial_port);
 
+/**
+ * USART1 interrupt handler
+ * Processes received data and transmission events for USART1
+ * Called automatically when USART1 triggers an interrupt
+ */
 void USART1_EXTI25_IRQHandler(void);
-
 #endif /* SERIAL_INTERRUPT_H_ */
