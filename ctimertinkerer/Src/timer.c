723392e216b1803551c4ef8c3f133d400f4aeeec
rroruu <roryleahclements@gmail.com>
2025-04-16 11:54:52 +1000

    fixing up timers to use timer getset properly

--- ctimertinkerer/Src/timer.c
+++ ctimertinkerer/Src/timer.c
@@ -7,18 +7,20 @@ static TimerCallback oneShotCallback = 0;
 static uint32_t timerPeriod_ms = 0;
 static uint8_t isOneShotMode = 0;
 
-void Timer_Init(uint32_t period_ms, TimerCallback cb) {
-    timerPeriod_ms = period_ms;
+void Timer_Init(TimerCallback cb) {
+    //timerPeriod_ms = period_ms;
     periodicCallback = cb;
 
     RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;
 
     TIM2->PSC = 8000 - 1;  // 1 ms tick
-    TIM2->ARR = timerPeriod_ms;
+    //TIM2->ARR = timerPeriod_ms;
+    uint32_t delay = 250;
+    setTimerPeriod(delay);
     TIM2->CNT = 0;
 
     TIM2->DIER |= TIM_DIER_UIE;  // Enable update interrupt
-    TIM2->EGR |= TIM_EGR_UG;
+    //TIM2->EGR |= TIM_EGR_UG;
     TIM2->SR &= ~TIM_SR_UIF;
 
     NVIC_EnableIRQ(TIM2_IRQn);
@@ -29,17 +31,19 @@ void Timer_Start(void) {
     TIM2->CR1 |= TIM_CR1_CEN;
 }
 
-void Timer_StartOneShot(uint32_t delay_ms, TimerCallback cb) {
+void Timer_StartOneShot(TimerCallback cb) {
     isOneShotMode = 1;
     oneShotCallback = cb;
 
     TIM2->CR1 &= ~TIM_CR1_CEN;      // Stop timer before configuring
     TIM2->PSC = 8000 - 1;           // 1 ms tick
-    TIM2->ARR = delay_ms;			// set overflow time
+    //TIM2->ARR = delay_ms;			// set overflow time
+    uint32_t delay = 1000;
+    setTimerPeriod(delay);
     TIM2->CNT = 0;					// start counting from 0
 
     TIM2->DIER |= TIM_DIER_UIE;		//setting other necessary timer vals
-    TIM2->EGR |= TIM_EGR_UG;
+    //TIM2->EGR |= TIM_EGR_UG;
     TIM2->SR &= ~TIM_SR_UIF;
 
     NVIC_EnableIRQ(TIM2_IRQn);		// enabling interrupt compatability
@@ -120,26 +124,26 @@ void initialise_board(void) {
 //SO THE FUNCTION CALLS WILL BE APPROPRIATELY NAMED AND DEFINED
 
 //uncomment this interrupt handler for one shot timing of the period outlined in the handler Timer_StartOneShot(XXXX, oneShotBlink);
-void EXTI0_IRQHandler(void) {
+/*void EXTI0_IRQHandler(void) {
     if (EXTI->PR & EXTI_PR_PR0) {
         EXTI->PR |= EXTI_PR_PR0;
 
-        uint32_t delay = 1000;
-        Timer_StartOneShot(delay, oneShotBlink);
+        //uint32_t delay = 1000;
+        Timer_StartOneShot(oneShotBlink);
     }
-}
+}*/
 
 //uncomment this interrupt handler for periodic timing of the period outlined in the main call Timer_Init(XXXX, periodicBlink);
-/*
+
 void EXTI0_IRQHandler(void) {
     if (EXTI->PR & EXTI_PR_PR0) { // check if flag for interrupt is set
         EXTI->PR |= EXTI_PR_PR0; // clear flag
 
-        Timer_Init(100, periodicBlink); //function callback using the arbitrary period of 1000, this sets and gets the period
+        Timer_Init(periodicBlink); //function callback using the arbitrary period of 1000, this sets and gets the period
         //change the above number to modify the period
         Timer_Start(); //start timing
 
         periodicBlink(); // Initiate immediate first toggle, then continue on
     }
 }
-*/
+
--- ctimertinkerer/Src/timer.h
+++ ctimertinkerer/Src/timer.h
@@ -5,10 +5,12 @@
 
 typedef void (*TimerCallback)(void);
 
-void Timer_Init(uint32_t period_ms, TimerCallback cb);
+void Timer_Init(TimerCallback cb);
 void Timer_Start(void);
 
-void Timer_StartOneShot(uint32_t delay_ms, TimerCallback cb);
+//void Timer_StartOneShot(uint32_t delay_ms, TimerCallback cb);
+void Timer_StartOneShot(TimerCallback cb);
+
 
 void setTimerPeriod(uint32_t new_period_ms);
 uint32_t getTimerPeriod(void);
